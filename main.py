2c7af7361179e085235eeda6797a3a00800c5ffc
JosephFlores <jflores9005@gmail.com>
2023-08-24 21:22:09 -0700

    main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
 
-    original_str = 'Python Programming'
+    original_str = 'Python Programming '
     ##################################################
     # Comlete your code here
     ##################################################
 
     sub1 = original_str[0:6]
-    sub2 = original_str[7:18]
+    sub2 = original_str[7:19]
     merged_str = sub2 + sub1
 
     print(sub2)
